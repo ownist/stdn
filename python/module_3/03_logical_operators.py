b15d5a28d4d3fce337a6bed9ccc9705ffe5c45d9
ownist <shahedevise@gmail.com>
2026-04-12 17:42:03 +0600

    learn(module_3): learn order of operations (PEMDAS) method
    - New file `04_order_of_operations_pemdas.py`
    - Explain PEMDAS method with examples
    - Update comment for `not` operator

--- python/module_3/03_logical_operators.py
+++ python/module_3/03_logical_operators.py
@@ -6,5 +6,5 @@ print(5 > 2 and 80 < 90)
 # logical or operator
 print(4 > 2 or 5 < 1)
 
-# not (reverse value like js ! sign, true thakle fasle and false thakle true)
+# not operator (reverse value like js ! sign, true thakle fasle and false thakle true)
 print(not (5 > 3))
---
+++ python/module_3/04_order_of_operations_pemdas.py
@@ -0,0 +1,18 @@
+# order of operations (PEMDAS method) ----------------------
+
+result = 3 + 4 * 2
+print(result)
+
+# PEMDAS method
+# P -> Parentheses
+# E -> Exponentiation / Exponents
+# M -> Multiplication
+# D -> Division
+# A -> Addition
+# S -> Substraction
+
+pemdas = (4 + 3) + (2**3) * 3  # 7 + 8 * 3 -> 7 + (8 * 3) -> 7 + 24 -> 31
+print(pemdas)
+
+a = (3 + 2) * 8  # 5 * 8 -> 40
+print(a)
