63e5c605851d1fa939bcb44d8cb197b4777a13ac
ownist <shahedevise@gmail.com>
2026-04-12 16:48:10 +0600

    learn(python/module_3): learn comparison operators
    - Added examples for basic comparison ops
    - Explained floor division in notes.md

---
+++ python/module_3/02_comparison_operators.py
@@ -0,0 +1,19 @@
+# basic comparison operators ------------------------
+
+# is equal to
+print(5 == 5)  # True
+
+# not equal to
+print(2 != 2)  # False
+
+# greater than
+print(6 > 3)  # True
+
+# less than
+print(5 < 10)  # True
+
+# greater than or equal (boro othoba soman)
+print(10 >= 10)  # True bcz dosh 10 er soman but boro na
+
+# less than or equal (choto othoba soman)
+print(20 <= 50)  # True bcz 20 50 er theke choto
