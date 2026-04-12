58c859d8c59da5a67caf85e0fbdc0cce526afb81
ownist <shahedevise@gmail.com>
2026-04-12 17:13:30 +0600

    learn(module_3 logical operators): learn logical operators and, or, not

--- python/module_3/03_logical_operators.py
+++ python/module_3/03_logical_operators.py
@@ -1,1 +1,10 @@
 # basic logical operators -----------------------------
+
+# logical and operator
+print(5 > 2 and 80 < 90)
+
+# logical or operator
+print(4 > 2 or 5 < 1)
+
+# not (reverse value like js ! sign, true thakle fasle and false thakle true)
+print(not (5 > 3))
