cb650471adb96d7c587b1447a545853d6e36b8e2
ownist <shahedevise@gmail.com>
2026-04-12 16:27:20 +0600

    learn(arithmetic): learn basic arithmetic operators

---
+++ python/module_3/arithmetic_operators.py
@@ -0,0 +1,21 @@
+
+# addition
+print(5 + 3)
+
+# substraction
+print(10 - 4)
+
+# multiplication
+print(5 * 7)
+
+# division
+print(10 / 2)
+
+# floor division
+print(10 // 7)
+
+# modulus
+print(85 % 3)
+
+# exponentiation
+print(2**3)
