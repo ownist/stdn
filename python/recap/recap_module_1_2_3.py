3e7c4395504378350308d0e93ab564e0e6f0fa4d
ownist <shahedevise@gmail.com>
2026-04-13 19:29:31 +0600

    recap: recap python module 1

---
+++ python/recap/recap_module_1_2_3.py
@@ -0,0 +1,7 @@
+# ------------------------ module 1 print function -----------------
+
+# print(20)
+# print(
+#     "i am shahed, and i learn programming to become a professional programmer, i dont care what people think about me, bcz who cares? manusher kotha sune ami jodi amr kaj ta na kori tahole din sesh ami nijei regret fell krbo.. jader kotha mone kore amr kaj ta jodi na kori tahole ki ora amake khawabe?"
+# )
+# print(5 + 10)
