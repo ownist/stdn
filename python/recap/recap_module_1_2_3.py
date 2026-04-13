7ad6bc319d1be5141a3dea910661efdf4cdfa9f6
ownist <shahedevise@gmail.com>
2026-04-13 19:55:23 +0600

    recap: recap module 03 content
    - Added arithmetic operators examples
    - Included comparison operators examples
    - Added logical operators examples
    - Documented order of operations (PEMDAS)

--- python/recap/recap_module_1_2_3.py
+++ python/recap/recap_module_1_2_3.py
@@ -54,10 +54,50 @@
 # print(name.upper())  # make it uppercase
 # print(language.lower())  # make it lowercase
 
-# ------------------- module 2 mini project ----------------------
+# module 2 mini project ----------------------
 # name = input("whats is your name: ")
 # age = input("how old are u: ")
 # is_student = input("are u student: ")
 
 # print(f"hey my name is {name}, i am {age} yrs old rn.")
 # print(f"is student: {is_student}")
+
+
+# --------------------- module 03 -----------------------
+#  arithmetic operators
+# print(512 + 658)  # addition
+# print(10 - 2)  # substraction
+# print(5 * 2)  # multiplication
+# print(10 / 2)  # division (with float) -> 5.0
+# print(50 // 2)  # floor division (integer) -> 25
+# print(54 % 15)  # modulus
+# print(4**3)  # exponentiation
+
+# comparison operator
+# print(5 == 5)  # True
+# print(5 == 4)  # False
+# print(10 != 20)  # True
+# print(4 > 2)  # True
+# print(80 < 100)  # True
+# print(20 >= 20)  # True
+# print(65 <= 65)  # True
+
+# basic logical operators
+# print(10 == 10 and 20 < 30)  # logical and operator
+# print(50 == 10 or 10 < 40)  # logical or operator
+
+# # logical not operator
+# print(not (20 > 10))
+# print(not (True))  # False
+# print(not (False))  # True
+
+# order of operations (PEMDAS) ----------
+pemdas = (2 + 5) + (2 * 3) * 3  # 7 + 6 * 3 -> 7 + (6 * 3) -> 7 + 18 -> 25
+print(pemdas)
+# PEMDAS method
+# P -> Parentheses
+# E -> Exponentiation / Exponents
+# M -> Multiplication
+# D -> Division
+# A -> Addition
+# S -> Substraction
