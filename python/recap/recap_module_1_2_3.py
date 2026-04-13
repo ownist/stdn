82c7febba743ebdd711d7905371e817b7ebe7cc3
ownist <shahedevise@gmail.com>
2026-04-13 19:29:45 +0600

    recap:  recap python module 2

--- python/recap/recap_module_1_2_3.py
+++ python/recap/recap_module_1_2_3.py
@@ -5,3 +5,59 @@
 #     "i am shahed, and i learn programming to become a professional programmer, i dont care what people think about me, bcz who cares? manusher kotha sune ami jodi amr kaj ta na kori tahole din sesh ami nijei regret fell krbo.. jader kotha mone kore amr kaj ta jodi na kori tahole ki ora amake khawabe?"
 # )
 # print(5 + 10)
+
+
+# ---------------------- module 02 --------------------------
+# variable---------
+# name = "ownist"
+# print(name)
+
+# age = 18
+# print(age)
+
+# data types ----------
+# name = "shahed (ownist)"  # string
+# age = 18  # integer (number)
+# height = 5.10  # float
+# is_student = True  # boolean
+# has_regret_for_past = False  # boolean
+
+# type convertion ----------
+# age = "18"
+# after_five_yrs = 5
+# print(int(age) + after_five_yrs)
+
+# integer to float convertion ----------
+# num = 15
+# convert_float = float(num)
+# print(num)  # 15 (pure integer)
+# print(convert_float)  # 15.0 (float)
+
+# age = 18
+# print(float(age))  # float type 18.0
+
+# string concatination ----------
+# first_name = "shahed"
+# last_name = "bishwas"
+
+# full_name = first_name + " " + last_name
+# print(full_name)
+
+# print(f"fullname: {first_name} {last_name}")  # using 'f' string
+
+# string method ----------
+# language = "PYTHON"
+# name = "shahed (ownist)"
+# print(len(language))  # count string length
+# print(len(name))
+
+# print(name.upper())  # make it uppercase
+# print(language.lower())  # make it lowercase
+
+# ------------------- module 2 mini project ----------------------
+# name = input("whats is your name: ")
+# age = input("how old are u: ")
+# is_student = input("are u student: ")
+
+# print(f"hey my name is {name}, i am {age} yrs old rn.")
+# print(f"is student: {is_student}")
