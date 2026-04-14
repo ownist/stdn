55e9d4dc14839db6ef3c5c8aff8f97fb5a098c47
ownist <shahedevise@gmail.com>
2026-04-14 15:45:27 +0600

    feat(activity): add basic sum calculator

---
+++ python/module_4/activity/basic_calculator.py
@@ -0,0 +1,6 @@
+# craete a basic calculator with input values
+
+first = int(input("Enter the first number: "))
+second = int(input("Enter the second number: "))
+
+print(f"{first} + {second} = {first + second}")
