2207934e08f6b9f519e7c5cb2ea6b0a1d7104bc4
ownist <shahedevise@gmail.com>
2026-04-14 15:00:12 +0600

    learn: ,earn simple if else and elif condition in python

---
+++ python/module_4/conditional_statements.py
@@ -0,0 +1,22 @@
+# if, elif, and else
+
+temperature = 10
+
+if temperature > 30:
+    print("it is a hot day")
+
+elif temperature > 20:
+    print("it is a warm day")
+
+else:
+    print("it is a cold day")
+
+
+# another example
+age = 18
+has_nid = True
+
+if age >= 18 and has_nid:
+    print("you can vote")
+else:
+    print("you cannot vote")
