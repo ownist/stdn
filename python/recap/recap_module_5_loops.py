97cfe536101e987d89040a418f650445afbcc618
ownist <shahedevise@gmail.com>
2026-04-18 23:02:20 +0600

    recap: recap for loop

--- python/recap/recap_module_5_loops.py
+++ python/recap/recap_module_5_loops.py
@@ -4,3 +4,17 @@ n = 1
 while n <= 5:
     print(n)
     n += 1
+
+print("\n")
+
+# ---------------------------- for loop ----------------------------
+for i in range(1, 6):
+    print(i)
+
+
+print("\n")
+
+
+for n in range(1, 11):
+    if n % 2 == 0:
+        print(n)
