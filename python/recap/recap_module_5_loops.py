f7f5f8b658632a2c22b710c52333935067f2b90c
ownist <shahedevise@gmail.com>
2026-04-18 23:06:11 +0600

    recap: recap list print using for loop

--- python/recap/recap_module_5_loops.py
+++ python/recap/recap_module_5_loops.py
@@ -18,3 +18,13 @@ print("\n")
 for n in range(1, 11):
     if n % 2 == 0:
         print(n)
+
+
+print("\n")
+
+
+# ---------------------------- list print using for loop ----------------------------
+my_teams = ["shahed", "rashel", "jihad", "tomjid"]
+
+for p in my_teams:
+    print(p.capitalize())
