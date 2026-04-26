c43c7e2e49dbdb89b3821e127d0bd2397973d490
ownist <shahedevise@gmail.com>
2026-04-26 07:31:23 +0600

    feat(function): add number addition
    - Introduce add_number function
    - Calculate and print sums
    - Comment out say_hello function

--- python/module_6/0_function.py
+++ python/module_6/0_function.py
@@ -12,9 +12,21 @@
 
 
 # parameter
-def say_hello(name, age):
-    print(f"Hey, I'm {name}!. RN I'm {age} yrs old.")
+# def say_hello(name, age):
+#     print(f"Hey, I'm {name}!. RN I'm {age} yrs old.")
 
 
-say_hello("ownist", 17)
-say_hello("rashel", 23)
+# say_hello("ownist", 17)
+# say_hello("rashel", 23)
+
+
+# return value
+def add_number(a, b):
+    return a + b
+
+
+sum = add_number(5, 2)
+print(sum)
+
+sum2 = add_number(4, 2)
+print(sum2)
