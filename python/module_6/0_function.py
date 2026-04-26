79eb615cb8a8ed5983266c40a7208732f328d818
ownist <shahedevise@gmail.com>
2026-04-26 07:31:08 +0600

    feat(function): add parameters to say_hello

--- python/module_6/0_function.py
+++ python/module_6/0_function.py
@@ -9,3 +9,12 @@
 
 # say_hello()
 # say_hello()
+
+
+# parameter
+def say_hello(name, age):
+    print(f"Hey, I'm {name}!. RN I'm {age} yrs old.")
+
+
+say_hello("ownist", 17)
+say_hello("rashel", 23)
