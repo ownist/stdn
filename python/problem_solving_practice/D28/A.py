4d5418ce66945059b0e3f49f1606a6e3931584c4
ownist <shahedevise@gmail.com>
2026-04-28 23:37:02 +0600

    feat(D28): add age calculation script

---
+++ python/problem_solving_practice/D28/A.py
@@ -0,0 +1,6 @@
+# ১. নাম ও বয়স: ইউজারের কাছ থেকে নাম এবং জন্ম সাল ইনপুট নিন, তারপর তার বর্তমান বয়স কত তা হিসাব করে প্রিন্ট করুন।
+
+name = input("Enter your name: ")
+date_of_birth = int(input("Date of Birth: "))
+
+print(f"{name}! you are {2026 - date_of_birth} yrs old.")
