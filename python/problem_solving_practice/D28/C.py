bcbaa3e3ec8ea8c8fb501d331e7dd9e66f23d0e2
ownist <shahedevise@gmail.com>
2026-04-28 23:43:38 +0600

    feat(D28): add problem solving scripts
    - Added name and age calculation script
    - Added temperature conversion script
    - Added even/odd number checker script

---
+++ python/problem_solving_practice/D28/C.py
@@ -0,0 +1,11 @@
+# জোড় না বিজোড়: একটি সংখ্যা ইনপুট নিয়ে সেটি জোড় নাকি বিজোড় তা পরীক্ষা করুন।
+
+
+def check_is_even_or_odd(num):
+    if num % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+
+check_is_even_or_odd(52)
