d54efd313c9df7c749e0994a349c68b75d477324
ownist <shahedevise@gmail.com>
2026-04-29 22:53:26 +0600

    feat(problem-solving): add time-based greeting script

---
+++ python/problem_solving_practice/D29/D.py
@@ -0,0 +1,9 @@
+# শুভেচ্ছা বার্তা: ইউজারের কাছ থেকে সময় (যেমন: সকাল, দুপুর বা রাত) ইনপুট নিন এবং সেই অনুযায়ী "শুভ সকাল" বা "শুভ রাত্রি" জানান।
+time = input("Enter current time: ")
+
+if time == "sokal":
+    print("shuvo sokal")
+elif time == "dupur":
+    print("lunch time")
+else:
+    print("shuvo ratri")
