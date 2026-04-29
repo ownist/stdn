e4efe12a5478b5ea1fabc5c71d449d3dfd984ce6
ownist <shahedevise@gmail.com>
2026-04-29 23:05:10 +0600

    feat(D29): add word length classifier

---
+++ python/problem_solving_practice/D29/I.py
@@ -0,0 +1,7 @@
+# বড় শব্দ না ছোট শব্দ: একটি শব্দ ইনপুট নিয়ে দেখুন তাতে ৫টির বেশি অক্ষর আছে কি না।
+word = input("Enter your fav word: ")
+
+if len(word) > 5:
+    print("yup.. big word")
+else:
+    print("small word")
