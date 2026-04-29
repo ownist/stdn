69008ff5504c515fbab0c423f5d7cb4205e3d9ac
ownist <shahedevise@gmail.com>
2026-04-29 23:09:13 +0600

    feat: Add K.py for name greeting practice
    - Implemented a list of 5 friend names
    - Used a loop to greet each friend

---
+++ python/problem_solving_practice/D29/K.py
@@ -0,0 +1,6 @@
+# তালিকা থেকে নাম: একটি লিস্টে ৫ জন বন্ধুর নাম রাখুন এবং লুপ ব্যবহার করে তাদের সবাইকে "হ্যালো" জানান।
+
+friends = ["shahed", "ownist", "sabbir", "bappi", "salim"]
+
+for i in friends:
+    print(f"hello {i}")
