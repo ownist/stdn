66f990025a7a861ac7fceee0e39d2c2ddb52f9ae
ownist <shahedevise@gmail.com>
2026-04-29 22:55:28 +0600

    feat(D29): add favorite color script

---
+++ python/problem_solving_practice/D29/E.py
@@ -0,0 +1,7 @@
+# পছন্দের রঙ: ইউজারের কাছে তার প্রিয় রঙ জানতে চান। যদি সেটি "লাল" হয় তবে বলুন "আমারও প্রিয়!", অন্য কিছু হলে বলুন "বাহ! সুন্দর রঙ।"
+color = input("Enter your fav color: ")
+
+if color == "red" or color == "lal":
+    print("amr o priyo")
+else:
+    print("bah! sundor rong...!")
