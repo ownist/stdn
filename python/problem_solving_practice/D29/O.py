80041eec57666d4cf94a43361325f6dcac54c6fa
ownist <shahedevise@gmail.com>
2026-04-29 23:20:59 +0600

    feat(list): print first and last elements

---
+++ python/problem_solving_practice/D29/O.py
@@ -0,0 +1,8 @@
+# লিস্টের প্রথম ও শেষ: একটি লিস্টের প্রথম এবং শেষ আইটেমটি প্রিন্ট করার জন্য একটি ফাংশন লিখুন।
+
+
+def first_and_last(list):
+    print(f"{list[0]}, {list[len(list) - 1]}")
+
+
+first_and_last(["shahed", "sabbir", "ownist", "niloy", "rachel kaka"])
