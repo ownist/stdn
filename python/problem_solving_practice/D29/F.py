216c2c04f4db13e595d85c4b7078e6ca4ef9f214
ownist <shahedevise@gmail.com>
2026-04-29 22:57:40 +0600

    feat(driving-license): add age eligibility check

---
+++ python/problem_solving_practice/D29/F.py
@@ -0,0 +1,8 @@
+# ড্রাইভিং লাইসেন্স: বয়স ইনপুট নিন। বয়স ১৮ বা তার বেশি হলে প্রিন্ট করুন "আপনি আবেদন করতে পারেন", নয়তো বলুন "এখনও সময় হয়নি"।
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("apni abedon korte parben")
+else:
+    print("ekhn o time hy nai")
