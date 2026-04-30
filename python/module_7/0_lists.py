074bf8cde45308b35d4a8f30cfc9a31ef93c3539
ownist <shahedevise@gmail.com>
2026-04-30 20:11:54 +0600

    feat(lists): add list methods examples
    - Added append, remove, pop methods
    - Added insert and sort methods
    - Added len function example

--- python/module_7/0_lists.py
+++ python/module_7/0_lists.py
@@ -9,3 +9,23 @@ print(fruits[2])
 print(fruits[2:])  # ['mango', 'lichi']
 print(fruits[:3])  # ['apple', 'banana', 'mango']
 print(fruits[1:3])  # ['banana', 'mango']
+
+print("\n")  # for new empty line
+
+# ---------- list method ----------
+fruits.append("orange")
+print(fruits)  # ['apple', 'banana', 'mango', 'lichi', 'orange']
+
+fruits.remove("banana")
+print(fruits)  # ['apple', 'mango', 'lichi', 'orange']
+
+fruits.pop()
+print(fruits)  # ['apple', 'mango', 'lichi']
+
+fruits.insert(0, "grave")
+print(fruits)  # ['grave', 'apple', 'mango', 'lichi']
+
+fruits.sort()
+print(fruits)  # ['apple', 'grave', 'lichi', 'mango']
+
+print(len(fruits))  # 4
