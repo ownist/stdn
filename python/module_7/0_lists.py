8f705ba31b35e165826aa0fde34bfa70ff7ad14e
ownist <shahedevise@gmail.com>
2026-04-30 19:30:31 +0600

    feat(lists): add basic list operations in python
    - Introduces a new python file for list examples
    - Demonstrates list creation and printing
    - Shows examples of list indexing and slicing

---
+++ python/module_7/0_lists.py
@@ -0,0 +1,11 @@
+# ---------- simple list overview ----------
+fruits = ["apple", "banana", "mango", "lichi"]
+print(fruits)
+
+# list indexing
+print(fruits[2])
+
+# list slicing
+print(fruits[2:])  # ['mango', 'lichi']
+print(fruits[:3])  # ['apple', 'banana', 'mango']
+print(fruits[1:3])  # ['banana', 'mango']
