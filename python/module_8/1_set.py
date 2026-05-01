fde9920a6cd58332d48bdb9ae2ec605c7302c493
ownist <shahedevise@gmail.com>
2026-05-02 00:14:28 +0600

    chore(set): add set operations examples

--- python/module_8/1_set.py
+++ python/module_8/1_set.py
@@ -1,3 +1,10 @@
 # introduce set
 colors = {"red", "green", "blue", "orange", "red"}
 print(colors)
+
+# set operation
+a = {1, 2, 3, 4}
+b = {3, 4, 5}
+print(a.union(b))
+print(a.intersection(b))
+print(a.difference(b))
