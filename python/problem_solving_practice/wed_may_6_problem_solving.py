2661104d09f601acd41e923fc3ebb5c0a838214a
ownist <shahedevise@gmail.com>
2026-05-07 22:43:51 +0600

    feat(problem-solving): add char counter without len()
    - Implemented string character counting
    - Avoids using built-in len() function
    - Iterates over string with a loop

--- python/problem_solving_practice/wed_may_6_problem_solving.py
+++ python/problem_solving_practice/wed_may_6_problem_solving.py
@@ -7,3 +7,20 @@ for n in range(1, 51):
 
 
 print(sum)
+
+print("\n")
+
+
+# ==========================
+# len() ফাংশন ব্যবহার না করে একটি লুপের সাহায্যে কোনো স্ট্রিংয়ে কতগুলো অক্ষর আছে তা গণনা করো।
+# ==========================
+def string_char_count(str):
+    count = 0
+
+    for char in str:
+        count += 1
+
+    return count
+
+
+print(string_char_count("ownist"))
