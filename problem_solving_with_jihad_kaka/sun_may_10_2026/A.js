f8563d7c434eb7f074ef291c1da9f32690c8f0ae
ownist <shahedevise@gmail.com>
2026-05-10 22:06:37 +0600

    feat(array): add totalSum function
    - Implemented totalSum utility function
    - Calculates sum of array elements
    - Includes example usage

--- problem_solving_with_jihad_kaka/sun_may_10_2026/A.js
+++ problem_solving_with_jihad_kaka/sun_may_10_2026/A.js
@@ -12,3 +12,22 @@ const checkisEvenOrOdd = (num) => {
 };
 
 console.log(checkisEvenOrOdd(56));
+
+console.log("\n");
+
+/**
+ * ==============================
+ * Find total sum of all numbers in an array.
+ * ==============================
+ */
+function totalSum(arr) {
+  let sum = 0;
+
+  for (const n of arr) {
+    sum += n;
+  }
+
+  return `total sum of numbers are: ${sum}`;
+}
+
+console.log(totalSum([1, 2, 3, 4]));
