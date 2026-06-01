df3c003f438b62ec2003229a97ea6c1cf5605afc
ownist <shahedevise@gmail.co.>
2026-06-01 20:48:10 +0600

    feat(ec): practice total sum of odd numbers
    - create a better comment to easily understandig

--- ec/ec.js
+++ ec/ec.js
@@ -1,4 +1,8 @@
-// monday, june 01
+/**
+ * =====================
+ * monday, june 01
+ * =====================
+*/
 function freqName(names){
   const freqUsers = {};
   
@@ -7,4 +11,26 @@ function freqName(names){
   }
 }
 
-freqName(['shahed', 'wahid', 'rachem kaka'])
+freqName(['shahed', 'wahid', 'rachem kaka']);
+
+console.log("\n") // create a new empty line for better understanding
+
+
+function sumOfOddNumsInOneToHundreed(nims){
+  let sum = 0;
+  
+  for(let i = 1; i <= 100; i++){
+    if(i % 2 !== 0){
+      sum += i;
+    }
+  }
+  
+  return sum;
+}
+
+console.log(sumOfOddNumsInOneToHundreed());
+/**
+ * =====================
+ * monday, june 01
+ * =====================
+ */
